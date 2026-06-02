8b413827f3772c805defd41169cf5f168ec1e39c
mustafa-ezzi <mustafaezzi143@gmail.com>
2026-06-02 23:51:19 +0500

    changes in frontend v2

--- frontend/src/api/services/adminAuthService.js
+++ frontend/src/api/services/adminAuthService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "backend-production-d32f.up.railway.app/api";
+
 
 const adminAuthService = {
   async login(payload) {
