2a4a3e0dd1678500b2a7f17ffd8ecc0dfe8f3413
mustafa-ezzi <mustafaezzi143@gmail.com>
2026-06-03 00:00:37 +0500

    changes in setting for cors header

--- samspython/samspython/settings.py
+++ samspython/samspython/settings.py
@@ -51,7 +51,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://localhost:5175",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://backend-production-d32f.up.railway.app",
+    "https://coreledger.up.railway.app"
 
 ]
 
